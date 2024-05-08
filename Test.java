4e19af756ae067682409ca801e656a00a4bf8066
pradoshcodes <pradoshmohanty034@gmail.com>
2024-05-08 13:00:20 +0530

    learning basics

---
+++ Test.java
@@ -0,0 +1,58 @@
+// public class Test{
+//     public static void main(String[] args){
+//     byte b=125;
+//     short s=25000;
+//     int price=45000;
+//     // int phno=9348568009;
+//     long mobile=934856760;
+//     System.out.println(b);
+//     System.out.println(s);
+//     System.out.println(price);
+//     // System.out.println(phno);
+//     System.out.println(mobile);
+//     }
+// }
+
+// public class Test{
+//     public static void main(String[] args){
+//     char b="Y";
+//     System.out.println(b);//Test.java:18: error: incompatible types: String cannot be converted to char
+//     //char b="Y";
+    
+//     }
+// }
+
+// public class Test{
+//     public static void main(String[] args){
+//     boolean b=true;
+//     boolean c=false;
+//     boolean d=null;
+
+//     System.out.println(b);
+//     System.out.println(c);
+//     System.out.println(d);
+    
+//     }//Test.java:28: error: incompatible types: <null> cannot be converted to boolean
+//     //boolean d=null;
+// }
+
+
+
+// public class Test{
+//         public static void main(String[] args){
+//         int ram=8,12;
+//         System.out.println(ram);//Test.java:42: error: <identifier> expected
+//         //int ram=8,12;if you store multiple value in primitive data types it show error 
+        
+//         }
+//     }
+
+
+class Test{
+    public static void main(String[] args) {
+        int[] num={20,30,40,25};
+        String[] name={"rahul","dinesh","vigor"};
+        System.out.println(num);
+        System.out.println(name);
+    }
+}
