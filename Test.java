b1fddb13530745294448f1f81ae6be13440f2cc7
pradoshcodes <pradoshmohanty034@gmail.com>
2024-05-09 12:55:31 +0530

    learn varibles

--- Test.java
+++ Test.java
@@ -1,58 +1,40 @@
-// public class Test{
-//     public static void main(String[] args){
-//     byte b=125;
-//     short s=25000;
-//     int price=45000;
-//     // int phno=9348568009;
-//     long mobile=934856760;
-//     System.out.println(b);
-//     System.out.println(s);
-//     System.out.println(price);
-//     // System.out.println(phno);
-//     System.out.println(mobile);
+// class Test{
+//     int eid1;//instance avrible
+//     static int eid2;//static varible
+
+//     public static void main(String[] args) {
+//         int eid3;//local varible 
+//         System.out.println(eid2);
 //     }
 // }
 
-// public class Test{
-//     public static void main(String[] args){
-//     char b="Y";
-//     System.out.println(b);//Test.java:18: error: incompatible types: String cannot be converted to char
-//     //char b="Y";
+// class Test{
+ 
+
+//     public static void main(String[] args) {
+//         int eid3;//local varible 
+//         System.out.println(eid3);//in local varible initialization is required but in static and instance varible it provide default values
+//     }
+// }
+
+// class Test{
+ 
+//   int a=100;
+//     public static void main(String[] args) {
+//        Test t = new Test();
+//         System.out.println(t.a);//to run this code we have to create an object of this instance varible
+//         //if we don;t create an object it gives compiler time error
+//     }
+// }
+
+
+
+/**
+ * Test
+ */
+public class Test {
+    static int a=100;
+public static void main(String[] args) {
     
-//     }
-// }
-
-// public class Test{
-//     public static void main(String[] args){
-//     boolean b=true;
-//     boolean c=false;
-//     boolean d=null;
-
-//     System.out.println(b);
-//     System.out.println(c);
-//     System.out.println(d);
-    
-//     }//Test.java:28: error: incompatible types: <null> cannot be converted to boolean
-//     //boolean d=null;
-// }
-
-
-
-// public class Test{
-//         public static void main(String[] args){
-//         int ram=8,12;
-//         System.out.println(ram);//Test.java:42: error: <identifier> expected
-//         //int ram=8,12;if you store multiple value in primitive data types it show error 
-        
-//         }
-//     }
-
-
-class Test{
-    public static void main(String[] args) {
-        int[] num={20,30,40,25};
-        String[] name={"rahul","dinesh","vigor"};
-        System.out.println(num);
-        System.out.println(name);
-    }
+}
 }
