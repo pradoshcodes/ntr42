2bb914a296fa0ae4b050ee5bd19b6ac3a17eae3d
pradoshcodes <pradoshmohanty034@gmail.com>
2024-05-31 10:34:40 +0530

    learning operator

---
+++ JS practice/operator.js
@@ -0,0 +1,41 @@
+//Addition : to append the value
+var a=10;
+var b=20.5;
+var c="30"
+var d="rahul"
+var e=true
+var f=false
+var g
+console.log(a*c);
+console.log(a*b);
+console.log(a*d);
+console.log(a*e);
+console.log(a*f);
+console.log(a*g);
+
+
+
+var a=10;
+var b=20.5;
+var c="30"
+var d="rahul"
+var e=true
+var f=false
+var g
+console.log(a+b);
+console.log(a+c);
+console.log(a+d);
+console.log(a+e);
+console.log(a+f);
+console.log(a+g);
+
+
+
+var a=10
+var b='10'
+var c=20
+
+console.log(a==b);
+console.log(a==c);//==(equality) is verify only content 
+console.log(a===b);//===(Strict equality) is verify both data type and content
+console.log(a===c);
