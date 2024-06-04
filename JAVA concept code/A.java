c2c176769ac69c588269c81a121944831f1e18b6
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-04 23:01:06 +0530

    learning access modifier

---
+++ JAVA concept code/A.java
@@ -0,0 +1,20 @@
+class A{
+    void m1(){
+        System.out.println("class A");
+
+    }
+
+}
+
+class B extends A{
+    void m1(){
+        System.out.println("class B");
+    }
+}
+
+class C extends A{
+    void m(){
+        System.out.println("class C");
+    }
+}
+
