cfe51875afbc9a8e6782677c37535a42180f92c2
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-06 07:12:11 +0530

    learn string concept

---
+++ Stringdemo.java
@@ -0,0 +1,9 @@
+public class Stringdemo {
+    public static void main(String[] args) {
+        String s1=new String("Rahul");
+        s1=s1.concat("Gandhi");
+        System.out.println(s1);
+        s1=s1.concat("Is good boy");
+        System.out.println(s1);
+    }
+}
