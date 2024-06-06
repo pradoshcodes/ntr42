b789a0971c1be564f283faf94318210b84581da4
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-06 07:21:35 +0530

    learning stringbuffer

--- Stringdemo.java
+++ Stringdemo.java
@@ -1,9 +1,17 @@
+// public class Stringdemo {
+//     public static void main(String[] args) {
+//         String s1=new String("Rahul");
+//         s1=s1.concat("Gandhi");
+//         System.out.println(s1);
+//         string s1=s1.concat("Is good boy");//here reference is required
+//         System.out.println(s1);
+//     }
+// }
+
 public class Stringdemo {
     public static void main(String[] args) {
-        String s1=new String("Rahul");
-        s1=s1.concat("Gandhi");
-        System.out.println(s1);
-        s1=s1.concat("Is good boy");
+        StringBuffer s1=new StringBuffer("rahul");
+        s1=s1.append("Gandhi");//reference is not required
         System.out.println(s1);
     }
 }
