8fd831a48a83327e9338798a1f5a5fc2ae67095e
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-13 22:20:23 +0530

    wrapper class

---
+++ JAVA concept code/Wrapper class/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        String eid="101";
+        int a=Integer.parseInt(eid);
+        Integer i=Integer.parseInt(eid);
+        System.out.println(a);
+        System.out.println(i);
+    }
+}
